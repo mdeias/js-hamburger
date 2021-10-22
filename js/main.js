0a8460c6e9f925dc325971431ff14287de200c10
mdeias <mdeias@icloud.com>
2021-10-22 17:10:18 +0200

    correzione variabili

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@
 // creo le funzioni 
 
 const menu = document.querySelector("div.hamburger-menu");
-const attivamenu = document.querySelector("div.hamburger-menu > a");
+const attivamenu = document.querySelector("div.header-right > a");
 const disattivamenu = document.querySelector("div.header-right > a");
+
+
+attivamenu.addEventListener("click", function(){
+    console.log("click rilevato");
+})
