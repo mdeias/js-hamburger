774efd45b0d84e63fe02685196b2277bbafb0cf1
mdeias <mdeias@icloud.com>
2021-10-22 17:22:40 +0200

    seconda funzione

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-// definisco le variabili
+// definisco le variabili: menu nascosto, icone di apertura e chiusura
 // creo le funzioni 
 
-const menu = document.querySelector("div.hamburger-menu");
+const menunascosto = document.querySelector("div.hamburger-menu");
 const attivamenu = document.querySelector("div.header-right > a");
 const disattivamenu = document.querySelector("div.hamburger-menu > a");
 
+// attivo il menù aggingendo la classe active tramite .add
 
 attivamenu.addEventListener("click", function(){
     console.log("click rilevato");
+    menunascosto.classList.add("active");
+})
+
+// disattivo il menù rimuovendo la classe active tramite .remove
+
+disattivamenu.addEventListener("click", function(){
+    console.log("click rilevato");
+    menunascosto.classList.remove("active");
 })
