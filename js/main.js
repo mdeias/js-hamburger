e531f54090cf2fcd5a2955ca68a82f33337817d6
mdeias <mdeias@icloud.com>
2021-10-22 17:02:00 +0200

    variabili

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// definisco le variabili
+// creo le funzioni 
+
+const menu = document.querySelector("div.hamburger-menu");
+const attivamenu = document.querySelector("div.hamburger-menu > a");
+const disattivamenu = document.querySelector("div.header-right > a");
